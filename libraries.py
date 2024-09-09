1968bbb6cd411a7d86c5d3163ed185b14a86eaae
zahraa <ftoomrrt@gmail.com>
2024-09-09 18:34:31 +0300

    libraries used

---
+++ libraries.py
@@ -0,0 +1,10 @@
+# Data processing
+import pandas as pd 
+import numpy as np 
+import scipy.stats
+# Visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+#similarity
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse import csr_matrix 
